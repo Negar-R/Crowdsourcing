ac6e3bf57f90c742434985fd7494c5d4e90c8599
Negar <negar.r.78.ioi@gmail.com>
2020-12-04 23:32:50 +0330

    override def_clean in form to restrict form_input

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.db.models import fields
+# from django.core.exceptions import ValidationError
 
 from tasks.models import TaskModel
 
@@ -16,5 +17,21 @@ class AddTaskForm(forms.ModelForm):
                                                  'id': 'reservationtime'}),
             
             'deadline': forms.DateTimeInput(attrs={'class': 'datetime-inputt-date',
-                                                   'autocomplete': 'off'}),
+                                                   'autocomplete': 'off',
+                                                   'placeholder': '2021-01-18'}),
         }
+
+    def clean(self):
+        value = self.cleaned_data.get('value')
+        estimation = self.cleaned_data.get('estimation')
+
+        if value < 1000 or value > 50000:
+            print("AVAL")
+            raise forms.ValidationError("Value should be between 1000 and 50000")
+        if estimation < 3 and value > 30000:
+            print("DOYOM")
+            raise forms.ValidationError(
+                "Maximum value for tasks with estimation lower than \
+                3 days is 30.000")
+                
+        return self.cleaned_data
--- tasks/permissions.py
+++ tasks/permissions.py
@@ -7,7 +7,6 @@ from accounts.models import UserProfile
 def is_agent(func):
     @functools.wraps(func)
     def wrapper(request, *args, **kwargs):
-        print('here')
         if request.user.userprofile.user_type == UserProfile.AGENT:
             return func(request, *args, **kwargs)
         else:
