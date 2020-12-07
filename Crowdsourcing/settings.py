9b55bf690d48267cddf5ede0d4514128b1e2798d
Negar <negar.r.78.ioi@gmail.com>
2020-12-07 16:42:00 +0330

    change static root

--- Crowdsourcing/settings.py
+++ Crowdsourcing/settings.py
@@ -204,9 +204,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # media files
