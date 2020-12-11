c864a4386ea9b3cb8d85fd426ddc564cc4277679
Negar <negar.r.78.ioi@gmail.com>
2020-12-11 15:08:55 +0330

    update .gitignore

--- Crowdsourcing/settings.py
+++ Crowdsourcing/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
-        'PORT': '',
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
